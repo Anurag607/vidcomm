6e2fdbd84448d0808ef1d3a0892bdbf42a9c79a6
Anurag607 <maskeddeep@gmail.com>
2023-08-31 10:05:07 +0530

    chore: project flow setup

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import '../widgets/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const routeName = '/';
+
+  const HomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import '../widgets/custom_video_player_preview.dart';
+
+class ProfileScreen extends StatelessWidget {
+  static const routeName = '/profile';
+
+  const ProfileScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
